e0bf5317a86fdcc44ddc2eec29b69f9ea4573faf
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-05-07 20:25:35 +0200

    Aktualizovat pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ A = 0
 B = 0
 C = 0
 
+#stisknutím tlačítek A a B povoluji/zakazuji hlasování
+#stisknutím tlačítka AB zobrazuji výsledky hlasování
+#stisknutím loga vynuluji výsledky hlasování
+
 def on_button_pressed_a():
     radio.send_number(1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
@@ -13,30 +17,39 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 
 def on_received_value(name, value):
-    radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
     global A, B, C
-    
-    if value == 65:
+    if name == "answer" and value == 1:
         A += 1
-    if value == 66:
+        radio.send_number(3)
+    if name == "answer" and value == 2:
         B += 1
-    if value == 67:
+        radio.send_number(3)
+    if name == "answer" and value == 3:
         C += 1
-
+        radio.send_number(3)
 radio.on_received_value(on_received_value)
 
-if input.button_is_pressed(Button.B):
-    radio.send_number(2)
+def on_button_pressed_ab():
+    global A, B, C
+    basic.show_string("A",500)
+    basic.show_number(A,500)
+    basic.show_string("B",500)
+    basic.show_number(B,500)
+    basic.show_string("C",500)
+    basic.show_number(C,500)
+    basic.show_icon(IconNames.HEART)
+    basic.clear_screen()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-    basic.show_number(A)
-    basic.pause(3000)
-    basic.show_number(B)
-    basic.pause(3000)
-    basic.show_number(C)
+def on_logo_event_pressed():
+    global A, B, C
+    A = 0
+    B = 0
+    C = 0
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+   
     
-console.log_value(65, A)
-console.log_value(66, B)
-console.log_value(67, C)
 
 
 
--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ radio.setGroup(33)
 let A = 0
 let B = 0
 let C = 0
+// stisknutím tlačítek A a B povoluji/zakazuji hlasování
+// stisknutím tlačítka AB zobrazuji výsledky hlasování
+// stisknutím loga vynuluji výsledky hlasování
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     radio.sendNumber(1)
 })
@@ -9,30 +12,37 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     radio.sendNumber(2)
 })
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    radio.receivedPacket(RadioPacketProperty.SignalStrength)
     
-    if (value == 65) {
+    if (name == "answer" && value == 1) {
         A += 1
+        radio.sendNumber(3)
     }
     
-    if (value == 66) {
+    if (name == "answer" && value == 2) {
         B += 1
+        radio.sendNumber(3)
     }
     
-    if (value == 67) {
+    if (name == "answer" && value == 3) {
         C += 1
+        radio.sendNumber(3)
     }
     
 })
-if (input.buttonIsPressed(Button.B)) {
-    radio.sendNumber(2)
-    basic.showNumber(A)
-    basic.pause(3000)
-    basic.showNumber(B)
-    basic.pause(3000)
-    basic.showNumber(C)
-}
-
-console.logValue(65, A)
-console.logValue(66, B)
-console.logValue(67, C)
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    basic.showString("A", 500)
+    basic.showNumber(A, 500)
+    basic.showString("B", 500)
+    basic.showNumber(B, 500)
+    basic.showString("C", 500)
+    basic.showNumber(C, 500)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    
+    A = 0
+    B = 0
+    C = 0
+})
